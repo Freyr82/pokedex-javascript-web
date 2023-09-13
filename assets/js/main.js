794b73a99db47dfa8d6b6f3f29209357efe6b8aa
Freyr821 <dunguelos36@gmail.com>
2023-09-13 19:00:58 -0300

    pokedexQuasePronta

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,45 @@
 
-console.log('Sucesso!')
+
+
+
+function convertPokemonToLi(pokemon) {
+    return `
+    <li class="pokemon ${pokemon.type}" >
+                <span class="Number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>               
+            </li>
+
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then ((pokemons = []) => {
+        pokemonList.innerHTML += pokemons.map (convertPokemonToLi).join('')
+})
+        // const newList = pokemons.map (convertPokemonToLi).join('')
+        // // (pokemon) => convertPokemonToLi(pokemon)
+        // const newHtml = newList
+
+        
+        
+        // const listItems = []
+
+        // for (let i = 0; i < pokemons.length; i++) {
+        //     const pokemon = pokemons[i];            
+        //     listItems.push(convertPokemonToLi(pokemon))
+            
+        // }
+        // console.log(listItems)
+    
+        
+    
+   
+
